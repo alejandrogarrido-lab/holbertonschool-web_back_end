84d644c6026680a4b70a3804bcd86d99d698fc73
alejandrogarrido-lab <alejandro_garrido@anahuacmayab.edu.mx>
2026-02-05 08:11:51 -0600

    NoSQL: fix 12-log_stats compatibility

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,17 +2,28 @@
 """ 12-log_stats """
 from pymongo import MongoClient
 
+
+def _count(coll, query=None):
+    """Count documents with compatibility across PyMongo versions."""
+    if query is None:
+        query = {}
+    try:
+        return coll.count_documents(query)
+    except Exception:
+        return coll.count(query)
+
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print("{} logs".format(collection.count_documents({})))
+    print("{} logs".format(_count(collection)))
     print("Methods:")
-    print("\tmethod GET: {}".format(collection.count_documents({"method": "GET"})))
-    print("\tmethod POST: {}".format(collection.count_documents({"method": "POST"})))
-    print("\tmethod PUT: {}".format(collection.count_documents({"method": "PUT"})))
-    print("\tmethod PATCH: {}".format(collection.count_documents({"method": "PATCH"})))
-    print("\tmethod DELETE: {}".format(collection.count_documents({"method": "DELETE"})))
+    print("\tmethod GET: {}".format(_count(collection, {"method": "GET"})))
+    print("\tmethod POST: {}".format(_count(collection, {"method": "POST"})))
+    print("\tmethod PUT: {}".format(_count(collection, {"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(_count(collection, {"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(_count(collection, {"method": "DELETE"})))
     print("{} status check".format(
-        collection.count_documents({"method": "GET", "path": "/status"})
+        _count(collection, {"method": "GET", "path": "/status"})
     ))
