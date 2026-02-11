7ea3a6d8b5874304765c6ac1ea6b4575241b0d7a
Alejandro Garrido <alejandro_garrido@anahuacmayab.edu.mx>
2026-02-11 17:14:19 +0000

    Fix uniq users table schema

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- Creates table users with unique email
+-- Creates users table with unique email
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
